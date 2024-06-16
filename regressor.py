7b72b7a5e13ba9720a0fed9921da4a04114f3a12
lapitskiy <lapithome@gmail.com>
2024-06-16 14:22:01 +0300

    lenta

--- regressor.py
+++ regressor.py
@@ -8,7 +8,7 @@ import gc
 import pandas as pd
 import numpy as np
 
-from sklearn.preprocessing import MinMaxScaler
+
 from sklearn.metrics import precision_recall_curve
 from sklearn.model_selection import train_test_split
 from sklearn.utils.class_weight import compute_class_weight
@@ -52,11 +52,10 @@ from tensorflow.keras.layers import LSTM, Dense, Dropout, LeakyReLU, Input, Bidi
 
 from tensorflow.keras import backend as K
 K.clear_session()
-import concurrent.futures
-from concurrent.futures import ProcessPoolExecutor, as_completed
+
 import logging
 import psutil
-from utils.path import create_dataframe, generate_uuid, path_exist, clear_folder, read_temp_path, save_grid_checkpoint, \
+from utils.path import generate_uuid, path_exist, clear_folder, read_temp_path, save_grid_checkpoint, \
     file_exist
 import matplotlib.pyplot as plt
 import matplotlib
@@ -409,35 +408,6 @@ def goLSTM(task):
     del x_train, x_val, y_train, y_val, x, y
     return 'End current epoch'
 
-def create_rolling_windows(df, df_scaled, current_threshold, input_window): # work BTC and TON and VOLUME
-    output_window = input_window  # Предсказываем на столько же периодов вперед, сколько и входных данных
-    num_samples = len(df) - input_window - output_window
-    feature_columns = numeric
-    num_features = len(feature_columns)
-
-    # Создание memmap файлов
-    uuid_mmap = generate_uuid()
-    if not os.path.exists('temp'):
-        os.makedirs('temp')
-    x_mmap = np.memmap(f'temp/mmap/{uuid_mmap}_x.dat', dtype=np.float32, mode='w+', shape=(num_samples, input_window, num_features))
-    y_mmap = np.memmap(f'temp/mmap/{uuid_mmap}_y.dat', dtype=np.int8, mode='w+', shape=(num_samples, output_window))
-
-    for i in range(num_samples):
-        if i % 20000 == 0:
-            print(f'create window {i} from {len(df)}')
-        x_mmap[i] = df_scaled[feature_columns].iloc[i:(i + input_window)].values
-        future_prices = df['close'].iloc[(i + input_window):(i + input_window + output_window)]
-        closing_price = df['close'].iloc[i + input_window - 1]
-        changes = (future_prices - closing_price) / closing_price
-        y_mmap[i] = (np.any(changes >= current_threshold)).astype(int)
-
-    # Синхронизация данных с диском и закрытие файлов
-    x_mmap.flush()
-    y_mmap.flush()
-    x_mmap._mmap.close()
-    y_mmap._mmap.close()
-    del x_mmap, y_mmap
-    return f'temp/mmap/{uuid_mmap}_x.dat', f'temp/mmap/{uuid_mmap}_y.dat', num_samples
 
 def f1_score(y_true, y_pred):
     # Первое, что нам нужно сделать, это убедиться, что обе переменные имеют тип float32
@@ -461,41 +431,6 @@ def f1_score(y_true, y_pred):
     # Обработка случая NaN в f1
     f1 = tf.where(tf.math.is_nan(f1), tf.zeros_like(f1), f1)
     return K.mean(f1)
-
-def process_data(df, current_window, current_threshold, current_period, numeric):
-    df.ffill(inplace=True)
-    df['pct_change'] = df['close'].pct_change(periods=current_window)
-    df['bullish_volume'] = df['volume'] * (df['close'] > df['open'])
-    df['bearish_volume'] = df['volume'] * (df['close'] < df['open'])
-    df[numeric] = df[numeric].astype(np.float32)
-    scaler = MinMaxScaler()
-    df_scaled = df.copy()
-    df_scaled[numeric] = scaler.fit_transform(df_scaled[numeric])
-    joblib.dump(scaler, f'temp/scaler/scaler_ct{current_threshold}_cw{current_window}_cp{current_period}.gz')
-    x_path, y_path, num_samples = create_rolling_windows(df, df_scaled, current_threshold, current_window)
-    try:
-        with open(f'temp/roll_win/roll_path_ct{current_threshold}_cw{current_window}_cp{current_period}.txt', 'w') as f:
-            f.write(x_path + '\n')
-            f.write(y_path + '\n')
-            f.write(str(num_samples) + '\n')
-    except Exception as e:
-        print(f"Failed to write paths to file: {e}")
-    return x_path, y_path
-
-def run_multiprocessing_rolling_window():
-    with ProcessPoolExecutor() as executor:
-        futures = []
-        for current_period in period:
-            df = create_dataframe(coin=coin, period=current_period, data=date_df)
-            for current_window in window_size:
-                for current_threshold in threshold:
-                    # Запускаем процесс
-                    futures.append(executor.submit(process_data, df.copy(), current_window, current_threshold, current_period, numeric))
-
-        # Обработка завершенных задач
-        for future in as_completed(futures):
-            x_path, y_path = future.result()
-            print(x_path, y_path)
 
 class ModelCheckpointWithMetricThreshold(Callback):
     def __init__(self, thresholds, filedata, directory_save, current_threshold, current_window, model, verbose=1):
@@ -584,7 +519,8 @@ if __name__ == '__main__':
     clear_folder('temp/mmap/')
 
 
-    run_multiprocessing_rolling_window()
+    run_multiprocessing_rolling_window(coin=coin, period=period, date_df=date_df, window_size=window_size,
+                                       threshold=threshold, numeric=numeric)
     #goKerasRegress
     if goKeras:
         goKerasRegressor(windows_size=window_size, thresholds=threshold, periods=period, dropouts=dropout, neirons=neiron)
---
+++ utils/rolling.py
@@ -0,0 +1,67 @@
+from utils.path import create_dataframe
+import concurrent.futures
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from sklearn.preprocessing import MinMaxScaler
+
+def run_multiprocessing_rolling_window(coin, period, date_df, window_size, threshold, numeric):
+    with ProcessPoolExecutor() as executor:
+        futures = []
+        for current_period in period:
+            df = create_dataframe(coin=coin, period=current_period, data=date_df)
+            for current_window in window_size:
+                for current_threshold in threshold:
+                    # Запускаем процесс
+                    futures.append(executor.submit(process_data, df.copy(), current_window, current_threshold, current_period, numeric))
+        # Обработка завершенных задач
+        for future in as_completed(futures):
+            x_path, y_path = future.result()
+
+def process_data(df, current_window, current_threshold, current_period, numeric):
+    df.ffill(inplace=True)
+    df['pct_change'] = df['close'].pct_change(periods=current_window)
+    df['bullish_volume'] = df['volume'] * (df['close'] > df['open'])
+    df['bearish_volume'] = df['volume'] * (df['close'] < df['open'])
+    df[numeric] = df[numeric].astype(np.float32)
+    scaler = MinMaxScaler()
+    df_scaled = df.copy()
+    df_scaled[numeric] = scaler.fit_transform(df_scaled[numeric])
+    joblib.dump(scaler, f'temp/scaler/scaler_ct{current_threshold}_cw{current_window}_cp{current_period}.gz')
+    x_path, y_path, num_samples = create_rolling_windows(df, df_scaled, current_threshold, current_window)
+    try:
+        with open(f'temp/roll_win/roll_path_ct{current_threshold}_cw{current_window}_cp{current_period}.txt', 'w') as f:
+            f.write(x_path + '\n')
+            f.write(y_path + '\n')
+            f.write(str(num_samples) + '\n')
+    except Exception as e:
+        print(f"Failed to write paths to file: {e}")
+    return x_path, y_path
+
+def create_rolling_windows(df, df_scaled, current_threshold, input_window): # work BTC and TON and VOLUME
+    output_window = input_window  # Предсказываем на столько же периодов вперед, сколько и входных данных
+    num_samples = len(df) - input_window - output_window
+    feature_columns = numeric
+    num_features = len(feature_columns)
+
+    # Создание memmap файлов
+    uuid_mmap = generate_uuid()
+    if not os.path.exists('temp'):
+        os.makedirs('temp')
+    x_mmap = np.memmap(f'temp/mmap/{uuid_mmap}_x.dat', dtype=np.float32, mode='w+', shape=(num_samples, input_window, num_features))
+    y_mmap = np.memmap(f'temp/mmap/{uuid_mmap}_y.dat', dtype=np.int8, mode='w+', shape=(num_samples, output_window))
+
+    for i in range(num_samples):
+        if i % 20000 == 0:
+            print(f'create window {i} from {len(df)}')
+        x_mmap[i] = df_scaled[feature_columns].iloc[i:(i + input_window)].values
+        future_prices = df['close'].iloc[(i + input_window):(i + input_window + output_window)]
+        closing_price = df['close'].iloc[i + input_window - 1]
+        changes = (future_prices - closing_price) / closing_price
+        y_mmap[i] = (np.any(changes >= current_threshold)).astype(int)
+
+    # Синхронизация данных с диском и закрытие файлов
+    x_mmap.flush()
+    y_mmap.flush()
+    x_mmap._mmap.close()
+    y_mmap._mmap.close()
+    del x_mmap, y_mmap
+    return f'temp/mmap/{uuid_mmap}_x.dat', f'temp/mmap/{uuid_mmap}_y.dat', num_samples
